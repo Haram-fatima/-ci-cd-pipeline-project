482f229247b0c6e00c3d7efc62a7470ff4eadb45
Haram-fatima <155131283+Haram-fatima@users.noreply.github.com>
2025-08-02 02:30:07 +0500

    Update deploy_frontend.sh

--- deploy/deploy_frontend.sh
+++ deploy/deploy_frontend.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-ssh jenkins@10.171.221.161 << 'ENDSSH'
+ssh jenkins@10.108.66.161 << 'ENDSSH'
 sudo mv /var/www/html /var/www/html_backup_$(date +%F_%T) || true
 sudo mkdir -p /var/www/html
 ENDSSH
 
-scp frontend.tar.gz jenkins@10.171.221.161:/tmp/
-ssh jenkins@10.171.221.161 << 'ENDSSH'
+scp frontend.tar.gz jenkins@10.108.66.161 :/tmp/
+ssh jenkins@10.108.66.161 << 'ENDSSH'
 sudo tar -xzf /tmp/frontend.tar.gz -C /var/www/html --strip-components=1
 sudo nginx -s reload
 ENDSSH
