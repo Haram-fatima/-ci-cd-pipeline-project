3de6ed856de73d2200ee02334c22fffa3967be03
Haram-fatima <155131283+Haram-fatima@users.noreply.github.com>
2025-08-02 02:29:09 +0500

    Update deploy_backend.sh

--- deploy/deploy_backend.sh
+++ deploy/deploy_backend.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-ssh jenkins@10.171.221.161 << 'ENDSSH'
+ssh jenkins@10.108.66.161 << 'ENDSSH'
 mkdir -p ~/app-backups
 cp -r ~/app-backend ~/app-backups/backup_$(date +%F_%T) || true
 rm -rf ~/app-backend
 mkdir ~/app-backend
 ENDSSH
 
-scp backend.tar.gz jenkins@10.171.221.161:~/app-backend/
-ssh jenkins@10.171.221.161 << 'ENDSSH'
+scp backend.tar.gz jenkins@10.108.66.161:~/app-backend/
+ssh jenkins@ 10.108.66.161<< 'ENDSSH'
 cd ~/app-backend
 tar -xzf backend.tar.gz
 cd dist
